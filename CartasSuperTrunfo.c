cf803fa2edf99e2bf4c841834a5ab09dee000353
João Santos <153041574+ShayMomoto@users.noreply.github.com>
2025-02-14 01:29:11 +0000

    Nivel Aventuriro - João Victor O Santos

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -30,6 +30,9 @@ int main() {
     printf ("Numero de Pontos Turisticos:\n");
     scanf ("%d", &pt);
 
+    float densidadePopulacional = (area > 0) ?(populacao / area): 0;
+    double pibPerCapita = (populacao > 0) ? (pib / populacao): 0;
+
       // Exibição dos dados cadastrados
     printf("\n===== Dados da Cidade =====\n");
     printf("Código: %s\n", codigo);
@@ -38,6 +41,7 @@ int main() {
     printf("Área: %.2f km²\n", area);
     printf("PIB: %.2lf bilhões\n", pib);
     printf("Pontos turísticos: %d\n", pt);
-
+    printf("Densidade populacional: %.2f hab/km²\n", densidadePopulacional);
+    printf("PIB per Capita: %.2lf bilhões/hab\n", pibPerCapita);
     return 0;
 }
